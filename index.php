7dc51e2e40e821988f4258514881889bcc831fda
admin <admin@iMac3.local>
2017-11-02 08:45:59 +0100

    templating effectif

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+require_once("./template/head.html");
+require_once("./template/header.html");
+require_once("./template/profil.html");
+require_once("./template/technology.html");
+require_once("./template/langues.html");
+require_once("./template/contact.html");
+require_once("./template/foot.html");
+
+
+// require_once("./pages/" . $_GET["page"] . ".html");
