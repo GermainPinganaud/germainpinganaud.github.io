18e1c50901e26716650ecd48d8f223cd4d6f1521
admin <admin@iMac3.local>
2017-11-03 16:51:04 +0100

    pret pour le week end, je veux ajouter mon jeu et traduire les pages

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 require_once("./template/head.html");
 require_once("./template/header.html");
 require_once("./template/profil.html");
-require_once("./template/technology.html");
-require_once("./template/contact.html");
+require_once("./template/travaux.html");
+require_once("./template/jsgame.html");
 require_once("./template/foot.html");
 
 
