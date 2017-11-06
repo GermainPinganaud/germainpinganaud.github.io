cfda79926f630426772db03658183317fcb1ccb0
admin <admin@iMac3.local>
2017-11-06 17:22:44 +0100

    template et 4 langues, php a faire

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
+session_start();
 require_once("./template/head.html");
 require_once("./template/header.html");
-require_once("./template/profil.html");
+if (!isset($_SESSION["language"])) {
+  $_SESSION["language"] = "Fr";
+}
+require_once("./template/profil" . $_SESSION["language"] . ".html");
 require_once("./template/travaux.html");
 require_once("./template/jsgame.html");
 require_once("./template/foot.html");
