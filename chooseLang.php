3d232472492360675c89a5cf58e3f5990f914cdb
admin <admin@iMac3-16.local>
2017-11-09 17:26:20 +0100

    add les suppression

--- chooseLang.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once("./template/head.html");
-require_once("./template/header.html");
-!isset($_SESSION["lang"]) ? $lang = "Fr" : $lang = $_GET["lang"];
-require_once("./template/profil" . $_SESSION["lang"] . ".html");
-require_once("./template/travaux.html");
-require_once("./template/jsgame.html");
-require_once("./template/foot.html");
